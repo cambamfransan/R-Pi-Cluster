f556605a2974ed42cab187d0434026dc2f451b03
Cameron Frandsen <cameron.frandsen1@gmail.com>
2017-12-17 12:53:00 -0700

    remove boost

--- Playground/main.cpp
+++ Playground/main.cpp
@@ -1,10 +1,7 @@
 #include <iostream>
-#include <boost/filesystem/path.hpp>
 
 int main()
 {
   std::cout << "Hello World" << std::endl;
-  boost::filesystem::path path("myFile.txt");
-  std::cout << path.string() << std::endl;
   return 0;
 }
