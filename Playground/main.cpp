758f75dc59df86b66920848456b27edaadcdcf26
Cameron Frandsen <cameron.frandsen1@gmail.com>
2017-12-17 10:24:59 -0700

    Use Boost

--- Playground/main.cpp
+++ Playground/main.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
-//#include <boost/filesystem/path.hpp>
+#include <boost/filesystem/path.hpp>
 
 int main()
 {
   std::cout << "Hello World" << std::endl;
-//  boost::filesystem::path path("myFile.txt");
-//  std::cout << path.string() << std::endl;
+  boost::filesystem::path path("myFile.txt");
+  std::cout << path.string() << std::endl;
   return 0;
 }
