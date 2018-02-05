47af36aac92533743add823df4ffb9562e6dd4a9
Cameron Frandsen <cameron.frandsen1@gmail.com>
2018-02-05 14:46:21 -0700

    we should use express

--- Web/main.js
+++ Web/main.js
@@ -1,23 +1,8 @@
+const express = require('express')
+const app = express()
 
-var http = require('http');
-var url = require('url');
-var fs = require("fs");
+app.get('/', (req, res) => res.sendFile(__dirname + '/MainPage.html'));
 
-var mainPage = function(response) {
-  return 'Main Page';
-}
+app.get('/pis', (req, res) => res.sendFile(__dirname + '/PiPage.html'));
 
-var piPage = function() {
-  return 'Raspberry pis';
-}
-
-var server = function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  var q = url.parse(req.url, true).query;
-  var txt;
-  if(req.url === '/')txt = mainPage(res);
-  else if(req.url == '/pi')txt = piPage();
-  res.end(txt);
-};
-
-http.createServer(server).listen(8080); //the server object listens on port 8080
+app.listen(80);
