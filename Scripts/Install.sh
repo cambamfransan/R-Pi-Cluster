a2ea23db9f4a62ff1c66220e7b59084922e057aa
Cameron Frandsen <cameron.frandsen1@gmail.com>
2018-02-08 14:07:26 -0700

    add in npm install and merge in python to test couts

--- Scripts/Install.sh
+++ Scripts/Install.sh
@@ -3,8 +3,11 @@
 mkdir ~/cluster
 cd ~/cluster
 git clone https://github.com/cambamfransan/R-Pi-Cluster.git
+git merge origin/python
 mkdir _bld
 cd _bld
+sudo apt-get update
+sudo apt-get update --fix-missing
 sudo apt-get install qt5-default -y
 sudo apt-get install rapidjson-dev -y
 sudo apt-get install libprotobuf-dev -y
@@ -12,5 +15,8 @@ sudo apt-get install cmake -y
 sudo apt-get install protobuf-compiler -y
 sudo apt-get install libprotobuf10 -y
 sudo apt-get install libprotoc-dev -y
+sudo apt-get install nodejs -y
+sudo apt-get install npm -y
+sudo npm install express --save
 cmake ../R-Pi-Cluster
 make -j3 install
