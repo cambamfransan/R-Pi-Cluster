736520e3a6e14de81fbd008d0c3b953c42cb0122
Cameron Frandsen <cameron.frandsen1@gmail.com>
2018-02-09 08:50:03 -0700

    it was not starting the client correctly

--- Scripts/fabfile.py
+++ Scripts/fabfile.py
@@ -4,5 +4,5 @@ def startClient(serverNode="127.0.0.1", port=0):
     run('git clone https://github.com/cambamfransan/R-Pi-Cluster.git') 
     run('sudo ./R-Pi-Cluster/Scripts/Install.sh')
 #	sudo("python test.py  2>/dev/null >/dev/null &")
-    run("./Client.exe " + serverNode + " " + port + " &")
+    run("./_bld/bin/Client.exe " + serverNode + " " + port + " &")
 
