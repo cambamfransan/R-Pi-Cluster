ca3d8a67fe5ef84dce14ff26034ceaf0c3a20731
Cameron Frandsen <cameron.frandsen1@gmail.com>
2018-02-09 08:34:40 -0700

    Update Scripts

--- Scripts/fabfile.py
+++ Scripts/fabfile.py
@@ -2,8 +2,7 @@ from fabric.api import run, env
 
 def startClient(serverNode="127.0.0.1", port=0):
     run('git clone https://github.com/cambamfransan/R-Pi-Cluster.git') 
-    run('chmod 777 R-Pi-Cluster/Scripts/Install.sh')
-    run('./R-Pi-Cluster/Scripts/Install.sh')
+    run('sudo ./R-Pi-Cluster/Scripts/Install.sh')
 #	sudo("python test.py  2>/dev/null >/dev/null &")
     run("./Client.exe " + serverNode + " " + port + " &")
 
