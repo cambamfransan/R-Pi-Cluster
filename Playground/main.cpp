20a44841bc2a0f9c6ad262773727932d9f539c3b
Cameron Frandsen <cameron.frandsen1@gmail.com>
2018-02-26 16:23:21 -0700

    how to run an external program

--- Playground/main.cpp
+++ Playground/main.cpp
@@ -1,10 +1,41 @@
-#include <QApplication>
+#include <cstdio>
 #include <iostream>
-#include <thread>
+#include <memory>
+#include <stdexcept>
+#include <string>
+#include <array>
 
-int main(int argc, char* argv[])
+#ifdef __unix
+std::string exec(const char* cmd) {
+  std::array<char, 128> buffer;
+  std::string result;
+  std::shared_ptr<FILE> pipe(popen(cmd, "r"), pclose);
+  if (!pipe) throw std::runtime_error("popen() failed!");
+  while (!feof(pipe.get())) {
+    if (fgets(buffer.data(), 128, pipe.get()) != nullptr)
+      result += buffer.data();
+  }
+  return result;
+}
+#else
+std::string exec(const char* cmd) {
+  std::array<char, 128> buffer;
+  std::string result;
+  std::shared_ptr<FILE> pipe(_popen(cmd, "r"), _pclose);
+  if (!pipe) throw std::runtime_error("_popen() failed!");
+  while (!feof(pipe.get())) {
+    if (fgets(buffer.data(), 128, pipe.get()) != nullptr)
+      result += buffer.data();
+  }
+  return result;
+}
+
+#endif
+
+int main()
 {
-  int a;
-  std::cin >> a;
+
+  std::cout << exec("dir") << std::endl;
+
   return 0;
 }
