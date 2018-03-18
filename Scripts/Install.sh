72092d4a52e6a51975dbfec301f8265f52657c57
cambamfransan <cameron.frandsen1@gmail.com>
2018-03-18 15:51:13 -0600

    minor modifications to install Script

--- Scripts/Install.sh
+++ Scripts/Install.sh
@@ -4,6 +4,7 @@ pwd
 sudo mkdir _bld
 sudo apt-get update
 sudo apt-get update --fix-missing
+sudo apt-get install vim
 sudo apt-get install qt5-default -y
 sudo apt-get install rapidjson-dev -y
 sudo apt-get install libprotobuf-dev -y
@@ -15,6 +16,10 @@ sudo apt-get install fabric -y
 sudo apt-get install nodejs -y
 sudo apt-get install npm -y
 ls
+git clone https://github.com/Tencent/rapidjson.git
+sudo cp rapidjson/include/rapidjson /usr/include/ -r
 cd ./R-Pi-Cluster/Web && sudo npm install express --save
 cd ./../../_bld && cmake ../R-Pi-Cluster
 make -j3 install
+nohup ./bin/Server &
+
