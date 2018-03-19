3ba3d4d3b0fe1b85d2458278904cde84f84e23ac
Cameron Frandsen <cameron.frandsen1@gmail.com>
2018-03-18 19:29:03 -0600

    use string as port and dont start server in start client

--- Scripts/fabfile.py
+++ Scripts/fabfile.py
@@ -1,6 +1,6 @@
 from fabric.api import run, env
 
-def startClient(serverNode="127.0.0.1", port=0):
+def startClient(serverNode="127.0.0.1", port="0"):
     run('git clone https://github.com/cambamfransan/R-Pi-Cluster.git') 
     run('sudo ./R-Pi-Cluster/Scripts/Install.sh')
 #	sudo("python test.py  2>/dev/null >/dev/null &")
