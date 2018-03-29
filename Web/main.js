2f0bb5fce885a8b8e24b2015fb999d5eee4b6497
Cameron Frandsen <cameron.frandsen1@gmail.com>
2018-03-29 15:20:04 -0600

    remove comments. If we need them, they are in the git history

--- Web/main.js
+++ Web/main.js
@@ -1,19 +1,12 @@
 var express = require('express');
 var appMain = express();
-//var appReset = express();
 var serverMain = require('http').Server(appMain);
-//var serverReset = require('http').Server(appReset);
 var fs = require('fs');
 
 appMain.get('/', function(req, res) {
     res.sendFile(__dirname + '/gui/index.html');
 });
 appMain.use('/gui', express.static(__dirname + '/gui'));
-
-// appReset.get('/', function(req, res) {
-// res.sendFile(__dirname + '/reset.html');
-// });
-// appReset.use('/', express.static(__dirname + '/'));
 
 var net = require('net');
 
@@ -22,7 +15,7 @@ client.connect(process.argv[2], '127.0.0.1', function() {
     console.log('Connected');
     client.write('Hello, server! Love, Client.');
 });
-/*
+
 client.on('data', function(data) {
     var temp = JSON.parse(data.toString());
     switch (temp.type) {
@@ -42,46 +35,10 @@ client.on('close', function() {
 });
 
 serverMain.listen(process.env.PORT || 8080); //console.log('Start mainServer'); // MAIN = 6246
-//serverReset.listen(process.env.PORT || 7378); //console.log('Start resetServer'); // REST = 7378
-
-//var systemData;
-
-/*fs.readFile('data/null.txt', 'utf8', function(error, data) {
-    if (error) {
-        //console.log('Read Error!');
-        systemData = null;
-    }
-    else {
-        //console.log('Read Success!');
-        systemData = data;
-    }
-});*/
 
 var ioMain = require('socket.io')(serverMain, {});
 ioMain.sockets.on('connection', function(socket) {
-//console.log('mainSocket Connected');
-//    socket.emit('systemData', systemData);
-socket.on('systemData', function(data) {
-    console.log("Received Data");
-});
-//        socket.emit('systemData', systemData);
-});
-
-/*var ioReset = require('socket.io') (serverReset,{});
-ioReset.sockets.on('connection', function(socket) {
-    //console.log('resetSocket Connected');
-    socket.on('resetSystem', function(data) {
-        fs.unlink('data/null.txt', function(error) {
-            if (error) {
-                //console.log('Delete Error!');
-                socket.emit('clearSystem', false);
-            }
-            else {
-                //console.log('Delete Success!');
-                systemData = null;
-                ioMain.emit('defaultSystem', systemData);
-                socket.emit('clearSystem', true);
-            }
-        });
+    socket.on('systemData', function(data) {
+        console.log("Received Data");
     });
-});*/
+});
